d2956b9f6c5f9fcdebf526bec72e9ef079e75d85
Alexander Castillo <alcastic@gmail.com>
2023-07-25 15:55:25 -0400

    refactor: sort folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 
-import Profile from './profile/profile'
-
+import Profile from '../components/profile/profile'
 import {userName} from './appconst'
 import {socialNetworkMap} from './appconst'
+import { Navbar } from '@/components/navbar/navbar'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Navbar/>
       <Profile userName={userName} socialNetworkMap={socialNetworkMap}/>
     </main>
   )
---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa"
+
+export function Navbar(){
+    return (
+        <div className="navbar">
+            <FaIcons.FaBars/>
+        </div>
+    )
+}
